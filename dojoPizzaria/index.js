732826ff8b9dfbf67883a6ea32de61002f3d2942
Marcos Macario <32857539+marvim1990@users.noreply.github.com>
2020-03-05 19:01:06 -0300

    organizando o git

--- dojoPizzaria/index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const app = express()
-const pizzaRota = require('./routes/pizzas')
-
-app.listen(3000, () => console.log("A pizzaria ta online!!!1!"));
-
-app.use(pizzaRota);
